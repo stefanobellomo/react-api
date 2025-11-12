11063648c2a28b574f6f5f00d89357db24d4c35e
stefanobellomo <stefanobellomo1998@gmail.com>
2025-11-12 13:28:26 +0100

    in app.jsx create row, col and actorCard, then map actors and add in tag html right value

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,22 @@ function App() {
   const apiActors = 'https://lanciweb.github.io/demo/api/actors/'
   const [actors, setActors] = useState([])
 
+  useEffect(handleClick, [])
+
+  // useEffect(() => { 
+  //   handleClick() 
+  // }, [])
+
   function handleClick() {
     axios.get(apiActors)
       .then(response => {
         console.log(response.data);
-
+        setActors(response.data)
       })
   }
+
+  console.log(actors);
+
 
 
   return (
@@ -22,14 +31,27 @@ function App() {
 
         <h1>Most popular actors</h1>
 
-        <button className="btn btn-dark" onClick={handleClick} >clicl me</button>
-
-        <ul>
-          <li>
-
-          </li>
-        </ul>
-
+        <button className="btn btn-dark" onClick={handleClick}>click me</button>
+        <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3">
+          {actors.map(actor => (
+            <div key={actor.id} className="col">
+              <div className="actorsCard">
+                <img src={actor.image} alt={actor.name} />
+                <h4>{actor.name}</h4>
+                <p className="d-flex flex-column">
+                  {/* anno nascita ed eventualmente di morte */}
+                  <span>Birth Year: {actor.birth_year} {actor.death_year ? `Death Year: ${actor.death_year}` : ''}</span>
+                  {/* nazionalità */}
+                  <span>{actor.nationality}</span>
+                  {/* biografia */}
+                  <span>{actor.biography}</span>
+                  {/* riconoscimenti */}
+                  <span>{actor.know_for}</span>
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
 
     </>
