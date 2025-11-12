945fc346a9f5e30507ddda3506a0fa4b2df49561
stefanobellomo <stefanobellomo1998@gmail.com>
2025-11-12 12:18:30 +0100

    MILESTONE 1   ---create basic rules in index.css ---in app.jsx, call api and useState for actors, then add html and customize button with onClick ---then create function handleClick and call axios.get... and pick from response data that contain every actors

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,34 @@ import { useEffect, useState } from "react"
 
 function App() {
 
+  // const apiActresses = 'https://lanciweb.github.io/demo/api/actresses/'
+  const apiActors = 'https://lanciweb.github.io/demo/api/actors/'
+  const [actors, setActors] = useState([])
+
+  function handleClick() {
+    axios.get(apiActors)
+      .then(response => {
+        console.log(response.data);
+
+      })
+  }
+
+
   return (
     <>
+      <div className="container">
+
+        <h1>Most popular actors</h1>
+
+        <button className="btn btn-dark" onClick={handleClick} >clicl me</button>
+
+        <ul>
+          <li>
+
+          </li>
+        </ul>
+
+      </div>
 
     </>
   )
